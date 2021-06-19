1a11416501c69ed8e58180c46ca21b20742c6472
Will Dale <w.dale@me.com>
2021-06-19 10:25:37 +0100

    Add ScrollView Demo.

--- Shared/Views/ChartViews/LineChart/LineChartDemoView.swift
+++ Shared/Views/ChartViews/LineChart/LineChartDemoView.swift
@@ -14,46 +14,53 @@ struct LineChartDemoView: View {
     let data: LineChartData = weekOfData()
     
     var body: some View {
-        
-        LineChart(chartData: data)
-            .extraLine(chartData: data, legendTitle: "Test") {
-                extraLineData
-            } style: {
-                extraLineStyle
+        ScrollView {
+            LineChart(chartData: data)
+                .extraLine(chartData: data, legendTitle: "Test") {
+                    extraLineData
+                } style: {
+                    extraLineStyle
+                }
+                .pointMarkers(chartData: data)
+                .touchOverlay(chartData: data, specifier: "%.0f")
+                .yAxisPOI(chartData: data,
+                          markerName: "Step Count Aim",
+                          markerValue: 15_000,
+                          labelPosition: .center(specifier: "%.0f"),
+                          labelColour: Color.black,
+                          labelBackground: Color(red: 1.0, green: 0.75, blue: 0.25),
+                          lineColour: Color(red: 1.0, green: 0.75, blue: 0.25),
+                          strokeStyle: StrokeStyle(lineWidth: 3, dash: [5,10]))
+                .yAxisPOI(chartData: data,
+                          markerName: "Minimum Recommended",
+                          markerValue: 10_000,
+                          labelPosition: .center(specifier: "%.0f"),
+                          labelColour: Color.white,
+                          labelBackground: Color(red: 0.25, green: 0.75, blue: 1.0),
+                          lineColour: Color(red: 0.25, green: 0.75, blue: 1.0),
+                          strokeStyle: StrokeStyle(lineWidth: 3, dash: [5,10]))
+                .averageLine(chartData: data,
+                             strokeStyle: StrokeStyle(lineWidth: 3, dash: [5,10]))
+                .xAxisGrid(chartData: data)
+                .yAxisGrid(chartData: data)
+                .xAxisLabels(chartData: data)
+                .yAxisLabels(chartData: data, colourIndicator: .style(size: 12))
+                .extraYAxisLabels(chartData: data, colourIndicator: .style(size: 12))
+                .infoBox(chartData: data)
+                .headerBox(chartData: data)
+                .legends(chartData: data, columns: [GridItem(.flexible()), GridItem(.flexible())])
+                .id(data.id)
+                .frame(minWidth: 150, maxWidth: 900, minHeight: 150, idealHeight: 500, maxHeight: 600, alignment: .center)
+                .padding(.horizontal)
+                
+                .navigationTitle("Week of Data")
+            
+            ForEach(data.dataSets.dataPoints, id: \.id) { point in
+                Text("\(point.value)")
+                Text("\(point.wrappedDescription)")
+                Divider()
             }
-            .pointMarkers(chartData: data)
-            .touchOverlay(chartData: data, specifier: "%.0f")
-            .yAxisPOI(chartData: data,
-                      markerName: "Step Count Aim",
-                      markerValue: 15_000,
-                      labelPosition: .center(specifier: "%.0f"),
-                      labelColour: Color.black,
-                      labelBackground: Color(red: 1.0, green: 0.75, blue: 0.25),
-                      lineColour: Color(red: 1.0, green: 0.75, blue: 0.25),
-                      strokeStyle: StrokeStyle(lineWidth: 3, dash: [5,10]))
-            .yAxisPOI(chartData: data,
-                      markerName: "Minimum Recommended",
-                      markerValue: 10_000,
-                      labelPosition: .center(specifier: "%.0f"),
-                      labelColour: Color.white,
-                      labelBackground: Color(red: 0.25, green: 0.75, blue: 1.0),
-                      lineColour: Color(red: 0.25, green: 0.75, blue: 1.0),
-                      strokeStyle: StrokeStyle(lineWidth: 3, dash: [5,10]))
-            .averageLine(chartData: data,
-                         strokeStyle: StrokeStyle(lineWidth: 3, dash: [5,10]))
-            .xAxisGrid(chartData: data)
-            .yAxisGrid(chartData: data)
-            .xAxisLabels(chartData: data)
-            .yAxisLabels(chartData: data, colourIndicator: .style(size: 12))
-            .extraYAxisLabels(chartData: data, colourIndicator: .style(size: 12))
-            .infoBox(chartData: data)
-            .headerBox(chartData: data)
-            .legends(chartData: data, columns: [GridItem(.flexible()), GridItem(.flexible())])
-            .id(data.id)
-            .frame(minWidth: 150, maxWidth: 900, minHeight: 150, idealHeight: 500, maxHeight: 600, alignment: .center)
-            .padding(.horizontal)
-        
-        .navigationTitle("Week of Data")
+        }
     }
     
     
